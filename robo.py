8bd348e6484061027de63387abce1e9206105e50
Jose Paschoal <paschoalvidiri@gmail.com>
2025-06-28 07:06:47 -0300

    Adiciona .gitignore para ignorar venv e arquivos temporários

--- robo.py
+++ robo.py
@@ -7,6 +7,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import pandas as pd
+
 import time
 
 caminho_driver = r"C:\Users\pasch\Desktop\chromedriver\chromedriver.exe"
@@ -24,12 +26,34 @@ driver.get(url)
 wait = WebDriverWait(driver, 15)
 # Espera os elementos com data-test='job-title' estarem presentes
 titulos = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a[data-test='job-title']")))
+empresas = driver.find_elements(By.CSS_SELECTOR, "span.EmployerProfile_compactEmployerName__9MGcV")
+locais = driver.find_elements(By.CSS_SELECTOR, "div[data-test='emp-location']")
+salarios = driver.find_elements(By.CSS_SELECTOR, "div[data-test='detailSalary']")
+publicacoes = driver.find_elements(By.CSS_SELECTOR, "div[data-test='job-age']")
 
 ## enxergando os resultados encontrados
 
-for titulo in titulos:
-    print(titulo.text)
+## criar lista de vagas
+
+vagas = []
+num_vagas = min(len(titulos), len(empresas), len(locais), len(publicacoes))
+
+
+for i in range(num_vagas):
+    vaga = {
+        'titulo': titulos[i].text,
+        'empresa': empresas[i].text,
+        'local': locais[i].text,
+        'pubicado_ha': publicacoes[i].text,
+        'salario': salarios[i].text if i < len(salarios) else 'Não informado'
+    }
+    vagas.append(vaga)
 
 
 driver.quit()  # Fecha o navegador
+
+## criar o dataframe
+
+df = pd.DataFrame(vagas)
+print(df.head())
 
